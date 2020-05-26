fb6b4ae3031c11a7aedf0ffad28f0131e05d3bb5
jeong-eui-jin <hehar1020@gmail.com>
2020-05-27 00:34:21 +0900

    settings extensions, app(moim) 추가

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # debug에서만
+    'django_extensions',
+
+    # add Apps
+    'moim'  # 메인 post 될 곳
+
 ]
 
 MIDDLEWARE = [
