a234cff559c1bac907389706ce5b90660611dab6
jeong-eui-jin <hehar1020@gmail.com>
2020-05-27 01:27:23 +0900

    moim App에 like 테이블 추가

--- django_app/moim/models.py
+++ django_app/moim/models.py
@@ -15,18 +15,49 @@ class TimeModel(models.Model):
         abstract = True
 
 
-# 모임 마스터 테이블
 class PostMaster(TimeModel):
+    """
+    모임 마스터 테이블
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    like_users = models.ManyToManyField(User, related_name="like_post")
+    like_users = models.ManyToManyField(User, related_name="like_posts", through='PostLike')
 
     def add_comment(self, user, content):
+        '''
+        댓글 바로 추가
+        :param user: 추가하는 유저
+        :param content: 댓글 내용
+        :return: 생성
+        '''
         return self.comment_set.create(user=user, content=content)
 
+    def like_count(self):
+        '''
+        모임 마스터 테이블의 좋아요 갯수
+        :return: 좋아요 갯수
+        '''
+        return self.postlike_set.count()
 
-# 모임 댓글 테이블
+
+class PostLike(TimeModel):
+    """
+    모임 마스터 좋아요 테이블
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(PostMaster, on_delete=models.CASCADE)
+
+
 class Comment(TimeModel):
+    """
+    모임 댓글 테이블
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     post = models.ForeignKey(PostMaster, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(User, related_name='like_comments', through='CommentLike')
+
+
+class CommentLike(TimeModel):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
